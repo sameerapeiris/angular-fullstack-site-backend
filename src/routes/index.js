ed408f419a2ff78a18fb8bd71e8f8aa6188765fb
sameerapeiris <sameedsdp@gmail.com>
2024-06-01 11:38:59 +0300

    update backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import { addViewToListingRoute } from "./addViewToListing";
 import { createNewListingRoute } from "./createNewListing";
+import { deleteListingRoute } from "./deleteListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { getUserListingsRoute } from "./getUserListings";
@@ -12,4 +13,5 @@ export default [
   getListingRoute,
   getUserListingsRoute,
   updateListingRoute,
+  deleteListingRoute
 ];
